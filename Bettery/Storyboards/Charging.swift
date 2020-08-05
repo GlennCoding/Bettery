fe607c14d9236c00c9728787415e18716b162374
Glenn Gregor <glenn.gregor.gg@gmail.com>
2020-08-05 14:23:27 +0200

    05.08.2020 - Storyborads + Design

---
+++ Bettery/Storyboards/Charging.swift
@@ -0,0 +1,30 @@
+//
+//  Charging.swift
+//  Bettery
+//
+//  Created by Glenn Gregor on 05.08.20.
+//  Copyright © 2020 Glenn Gregor. All rights reserved.
+//
+
+import UIKit
+
+class Charging: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Bettery/Storyboards/Homescreen.swift
@@ -0,0 +1,42 @@
+//
+//  Homescreen.swift
+//  Bettery
+//
+//  Created by Glenn Gregor on 04.08.20.
+//  Copyright © 2020 Glenn Gregor. All rights reserved.
+//
+
+import UIKit
+
+class Homescreen: UIViewController {
+
+    @IBOutlet weak var CurrentBatteryPercentage: UILabel!
+    
+    
+    
+    var batteryLevel: Float { UIDevice.current.batteryLevel }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print(batteryLevel)
+        CurrentBatteryPercentage.text = "\(Int(batteryLevel * 100))%"
+    }
+    
+    @objc func batteryLevelDidChange(_ notification: Notification) {
+        print(batteryLevel)
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
